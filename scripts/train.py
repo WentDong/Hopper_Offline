1daa8b242169b9fd72225a31d4c4cb840af4436d
Dong Wentao <1134136579@qq.com>
2023-04-20 15:42:40 +0800

    Simple BC

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,19 @@
 import torch
 import numpy as np
 from scripts.dataloader import D4RLTrajectoryDataset
+from scripts.args import get_args
+from agents.bc.bc_agent import BC
+from torch.utils.data import DataLoader
+def train():
+	pass
 
 if __name__ == "__main__":
-    args = get_args()
-    dataset = D4RLTrajectoryDataset(args.dataset_path, args.file_name)
-    
+	args = get_args()
+	dataset = D4RLTrajectoryDataset(args.dataset_path, args.file_name)
+	data_loader = DataLoader(
+		dataset,
+		bacth_size=args.batch_size,
+		shuffle = True
+	)
+	model = BC(state_dim=11, action_dim=3, hidden_dim=128)
+	train(args, model, data_loader)
