ec5a2df1d33efc2a20b96107039bccf3f6429954
Dong Wentao <1134136579@qq.com>
2023-05-21 00:19:00 +0800

    add codes

---
+++ agents/AWR/submission.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+from pathlib import Path
+import sys
+import torch
+base_dir = Path(__file__).resolve().parent
+sys.path.append(str(base_dir))
+from AWR_agent import AWR
+
+agent = AWR(state_dim=11, action_dim=3, hidden_dim=512)
+AWR_net = os.path.dirname(os.path.abspath(__file__)) + "/AWR_best.pth"
+state_dict=  torch.load(AWR_net)
+agent.load_state_dict(state_dict)
+def my_controller(observation, action_space, is_act_continuous=True):
+    action = agent.take_action(observation['obs'])
+    return [action]
---
+++ scripts/env_render.py
@@ -0,0 +1,45 @@
+import gym
+from gym.envs.mujoco.hopper import HopperEnv
+from dataloader import TrajectoryDataset
+from args import get_args
+import numpy as np
+class MyHopperEnv(HopperEnv):
+	def reset_model(self, state = None):
+		if state is None:
+			qpos = self.init_qpos + self.np_random.uniform(
+            low=-0.005, high=0.005, size=self.model.nq
+			)
+			qvel = self.init_qvel + self.np_random.uniform(
+				low=-0.005, high=0.005, size=self.model.nv
+			)
+			self.set_state(qpos, qvel)
+			return self._get_obs()
+		else:
+			qpos = np.array([0.] + state[:5].tolist())
+			qvel = state[5:]
+			self.set_state(qpos, qvel)
+			return self._get_obs()
+
+if __name__ =="__main__":
+	env = MyHopperEnv()
+	state = env.reset()
+	print(state)
+	print(env.init_qpos, env.init_qvel)
+	print(env.model.nq, env.model.nv)
+	print(env.sim.get_state())
+
+	args = get_args()
+	dataset = TrajectoryDataset(args.dataset_path, args.file_name)
+	Traj = dataset.__getitem__(0)
+	state = Traj[0][0]
+	print(state,state.shape)
+	env.reset_model(state)
+	env.render()
+	for i in range(len(Traj)):
+		action = Traj[i][1]
+
+		next_state, reward, done, _ = env.step(action)
+		print(next_state-Traj[i][2])
+		print(reward-Traj[i][3])
+		env.render()
+
