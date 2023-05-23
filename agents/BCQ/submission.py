23de51a2b8b313c114a3fd8801231436b85c56de
Dong Wentao <1134136579@qq.com>
2023-05-24 03:50:48 +0800

    add submission

---
+++ agents/BCQ/submission.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+from pathlib import Path
+import sys
+import torch
+base_dir = Path(__file__).resolve().parent
+sys.path.append(str(base_dir))
+from bcq_agent import BCQ
+
+agent = BCQ()
+BCQ_net = os.path.dirname(os.path.abspath(__file__)) + "/BCQ_best.pth"
+state_dict=  torch.load(BCQ_net)
+agent.load_state_dict(state_dict)
+def my_controller(observation, action_space, is_act_continuous=True):
+    action = agent.take_action(observation['obs'])
+    return [action]
